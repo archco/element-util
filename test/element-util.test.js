9be18bdc252744fe34716f3539fac7e53bee0d8e
archco <archcoster@gmail.com>
2017-10-06 21:36:02 +0900

    Modify test.

--- test/element-util.test.js
+++ test/element-util.test.js
@@ -1,7 +1,4 @@
-// const chai = require('chai');
-// chai.use(require('chai-dom'));
-// const expect = chai.expect;
-const expect = chai.expect;
+const expect = window.chai.expect;
 const ElementUtil = window.ElementUtil.default;
 
 describe('ElementUtil', () => {
@@ -9,38 +6,54 @@ describe('ElementUtil', () => {
     expect(ElementUtil).to.be.an('object');
   });
 
-  describe('#getElement', () => {
-    it('Returns to be an instance of Element.', () => {
-      let elm = ElementUtil.getElement('#mocha');
-      expect(elm).to.be.an.instanceof(Element);
-    });
+  describe('baseMethods', () => {
+    describe('#getElement', () => {
+      it('Returns to be an instance of Element.', () => {
+        let elm = ElementUtil.getElement('#mocha');
+        expect(elm).to.be.an.instanceof(Element);
+      });
 
-    it('If base is not exists, then will occur ReferenceError.', () => {
-      let badFn = () => ElementUtil.getElement('div', '#mocha1');
-      expect(badFn).to.throw(ReferenceError);
-    });
-  });
-
-  describe('#filter', () => {
-    it('filtering on table element.', () => {
-      let hit = ElementUtil.filter('table.filter', 'james');
-      expect(hit).to.equal(1);
+      it('If base is not exists, then will occur ReferenceError.', () => {
+        let badFn = () => ElementUtil.getElement('div', '#mocha1');
+        expect(badFn).to.throw(ReferenceError);
+      });
     });
   });
 
-  describe('#sort', () => {
-    it('sort on list.', () => {
-      let items = ElementUtil.sort('ul.sort');
-      expect(items[0].textContent).to.equal('abc');
+  describe('utilMethods', () => {
+    describe('#addClass', () => {
+      it('Add class to element.', () => {
+        let elm = ElementUtil.getElement('#test');
+        ElementUtil.addClass(elm, 'test-class');
+        expect(elm.classList.contains('test-class')).to.equal(true);
+      });
     });
+  });
 
-    it('sort on table.', () => {
-      ElementUtil.sort('table.sort');
-      ElementUtil.getElement('table.sort thead th:nth-child(3)').click();
-      let firstRowDate = ElementUtil
-        .getElement('td:nth-child(3)', 'table.sort tbody tr:first-child')
-        .textContent;
-      expect(firstRowDate).to.equal('1444-05-01');
+  describe('ElementFilter', () => {
+    describe('#filter', () => {
+      it('filtering on table element.', () => {
+        let hit = ElementUtil.filter('table.filter', 'james');
+        expect(hit).to.equal(1);
+      });
+    });
+  });
+
+  describe('ElementSort', () => {
+    describe('#sort', () => {
+      it('sort on list.', () => {
+        let items = ElementUtil.sort('ul.sort');
+        expect(items[0].textContent).to.equal('abc');
+      });
+
+      it('sort on table.', () => {
+        ElementUtil.sort('table.sort');
+        ElementUtil.getElement('table.sort thead th:nth-child(3)').click();
+        let firstRowDate = ElementUtil
+          .getElement('td:nth-child(3)', 'table.sort tbody tr:first-child')
+          .textContent;
+        expect(firstRowDate).to.equal('1444-05-01');
+      });
     });
   });
 });
