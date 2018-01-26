4f58bae1d199cb7d935207f8ac675eceb775669f
archco <archcoster@gmail.com>
2018-01-26 14:37:00 +0900

    fix references.. again

--- src/element-util.d.ts
+++ src/element-util.d.ts
@@ -1,3 +1,9 @@
+/// <reference types="../node_modules/typescript/lib/lib.dom" />
+/// <reference types="../node_modules/typescript/lib/lib.es5" />
+/// <reference types="../node_modules/typescript/lib/lib.scripthost" />
+/// <reference types="../node_modules/typescript/lib/lib.dom.iterable" />
+/// <reference types="../node_modules/typescript/lib/lib.es2015.iterable" />
+
 import ElementFilter from './classes/element-filter';
 import ElementSorter from './classes/element-sorter';
 import * as BaseMethods from './methods/base';
