d65d6751b13605f29fec077c3542d108a52e445e
archco <archcoster@gmail.com>
2018-01-27 10:28:53 +0900

    Remove old files.

--- test/test.ts
+++
@@ -1,9 +0,0 @@
-import eu, { ElementFilter as ef, ElementSorter as es } from '../types/element-util.d';
-
-const f = new ef('#app', 'h', { enableHTML: true });
-const s = new es('ul', {
-  items: 'auto',
-  datasetName: {
-    sortType: 'sortType',
-  },
-});
