6a9c2542efde0ed6bda7cb5eae377861383c2c4a
archco <archcoster@gmail.com>
2018-02-23 10:41:00 +0900

    Update declaration file. #5

--- src/element-util.d.ts
+++ src/element-util.d.ts
@@ -1,187 +1,185 @@
-export default ElementUtil;
-export { ElementFilter, ElementSorter };
+export as namespace ElementUtil;
+
+//
+// Base methods.
+//
 
 declare type ElementTarget = string|Element|NodeList;
 
-declare namespace ElementUtil {
-  //
-  // Base methods.
-  //
-
-  /**
-   * Get element.
-   * @param  selector querySelector
-   * @param  base     base element. default is Document.
-   * @return
-   */
-  export function getElement(selector: ElementTarget, base?: Document|ElementTarget): Element;
-
-  /**
-   * Get elements as NodeList.
-   * @param  selector querySelector
-   * @param  base     base element. default is Document.
-   * @return
-   */
-  export function getElements(selector: ElementTarget, base?: Document|ElementTarget): NodeList;
-
-  /**
-   * Remove elements.
-   * @param  selector querySelector
-   * @param  base     base element. default is Document.
-   * @return          number of removed.
-   */
-  export function removeElements(selector: ElementTarget, base?: Document|ElementTarget): number;
-
-  /**
-   * Convert a single element to NodeList.
-   * @param  elm
-   * @return
-   */
-  export function toNodeList(elm: Element|string): NodeList;
-
-  /**
-   * Convert NodeList to Array.
-   * @param  list
-   * @return
-   */
-  export function nodeListToArray(list: NodeList|any[]|string): any[];
-
-  /**
-   * Find ancestor element.
-   * @param  self
-   * @param  ancestor
-   * @return          Element or null.
-   */
-  export function findAncestor(self: ElementTarget, ancestor: ElementTarget): Element|null;
-
-  //
-  // Util methods.
-  //
-
-  /**
-   * Add event listener on every selected elements.
-   * @param  selector   querySelector
-   * @param  type       event-type
-   * @param  listener   listener
-   * @param  useCapture default is false
-   * @return            number of affected
-   */
-  export function addListener(
-    selector: ElementTarget,
-    type: string,
-    listener: EventListener,
-    useCapture?: boolean,
-  ): number;
-
-  /**
-   * Wrapping each element.
-   * @param selector  querySelector
-   * @param className wrapper's class name.
-   * @param tagName   wrapper's tag name, default is 'div'.
-   */
-  export function wrap(selector: ElementTarget, className: string, tagName?: string): void;
-
-  /**
-   * Wrapping all elements inside a one wrapper.
-   * @param selector  querySelector
-   * @param className wrapper's class name.
-   * @param tagName   wrapper's tag name, default is 'div'.
-   */
-  export function wrapAll(selector: ElementTarget, className: string, tagName?: string): void;
-
-  /**
-   * Add confirm message listener when 'submit' event.
-   * @param selector querySelector, form element probably.
-   * @param message  confirm message.
-   */
-  export function submitConfirm(selector: ElementTarget, message?: string): void;
-
-  /**
-   * Add class to element.
-   * @param selector  querySelector
-   * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
-   */
-  export function addClass(selector: ElementTarget, className: string): void;
-
-  /**
-   * Remove class from element.
-   * @param selector  querySelector
-   * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
-   */
-  export function removeClass(selector: ElementTarget, className: string): void;
-
-  /**
-   * Toggling class to element.
-   * @param selector  querySelector
-   * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
-   */
-  export function toggleClass(selector: ElementTarget, className: string): void;
-
-  /**
-   * Hide element. It will set element's display to 'none'.
-   * @param selector querySelector
-   */
-  export function hide(selector: ElementTarget): void;
-
-  /**
-   * Show element. It will just remove 'display=none;'.
-   * @param selector querySelector
-   */
-  export function show(selector: ElementTarget): void;
-
-  /**
-   * Toggling show/hide element.
-   * @param selector querySelector
-   */
-  export function toggleShow(selector: ElementTarget): void;
-
-  /**
-   * Make a hidden input.
-   * @param  name  name attribute.
-   * @param  value value attribute.
-   * @return
-   */
-  export function makeHiddenInput(name: string, value: string): HTMLInputElement;
-
-  /**
-   * Makes hidden input, and append to target element. If input[name=".."] already exists, overwrite it.
-   * @param target querySelector
-   * @param name   name attribute.
-   * @param value  value attribute.
-   */
-  export function appendHiddenInput(target: ElementTarget, name: string, value: string): void;
-
-  //
-  // Helper methods.
-  //
-
-  /**
-   * Filtering elements.
-   * @param  selector   querySelector
-   * @param  filter     filter string.
-   * @param  enableHTML using .innerHTML, default is false.
-   * @return            Hit number.
-   */
-  export function filter(selector: ElementTarget, filter?: string, enableHTML?: boolean): number;
-
-  /**
-   * Sorting elements.
-   * @param  elm     base element.
-   * @param  options options for ElementSorter.
-   * @return         sorted elements.
-   */
-  export function sort(elm: ElementTarget, options?: SorterOptions): HTMLElement[];
-}
+/**
+ * Get element.
+ * @param  selector querySelector
+ * @param  base     base element. default is Document.
+ * @return
+ */
+export function getElement(selector: ElementTarget, base?: Document|ElementTarget): Element;
 
 /**
- * ElementFilter
+ * Get elements as NodeList.
+ * @param  selector querySelector
+ * @param  base     base element. default is Document.
+ * @return
  */
+export function getElements(selector: ElementTarget, base?: Document|ElementTarget): NodeList;
+
+/**
+ * Remove elements.
+ * @param  selector querySelector
+ * @param  base     base element. default is Document.
+ * @return          number of removed.
+ */
+export function removeElements(selector: ElementTarget, base?: Document|ElementTarget): number;
+
+/**
+ * Convert a single element to NodeList.
+ * @param  elm
+ * @return
+ */
+export function toNodeList(elm: Element|string): NodeList;
+
+/**
+ * Convert NodeList to Array.
+ * @param  list
+ * @return
+ */
+export function nodeListToArray(list: NodeList|any[]|string): any[];
+
+/**
+ * Find ancestor element.
+ * @param  self
+ * @param  ancestor
+ * @return          Element or null.
+ */
+export function findAncestor(self: ElementTarget, ancestor: ElementTarget): Element|null;
+
+//
+// Util methods.
+//
+
+/**
+ * Add event listener on every selected elements.
+ * @param  selector   querySelector
+ * @param  type       event-type
+ * @param  listener   listener
+ * @param  useCapture default is false
+ * @return            number of affected
+ */
+export function addListener(
+  selector: ElementTarget,
+  type: string,
+  listener: EventListener,
+  useCapture?: boolean,
+): number;
+
+/**
+ * Wrapping each element.
+ * @param selector  querySelector
+ * @param className wrapper's class name.
+ * @param tagName   wrapper's tag name, default is 'div'.
+ */
+export function wrap(selector: ElementTarget, className: string, tagName?: string): void;
+
+/**
+ * Wrapping all elements inside a one wrapper.
+ * @param selector  querySelector
+ * @param className wrapper's class name.
+ * @param tagName   wrapper's tag name, default is 'div'.
+ */
+export function wrapAll(selector: ElementTarget, className: string, tagName?: string): void;
+
+/**
+ * Add confirm message listener when 'submit' event.
+ * @param selector querySelector, form element probably.
+ * @param message  confirm message.
+ */
+export function submitConfirm(selector: ElementTarget, message?: string): void;
+
+/**
+ * Add class to element.
+ * @param selector  querySelector
+ * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
+ */
+export function addClass(selector: ElementTarget, className: string): void;
+
+/**
+ * Remove class from element.
+ * @param selector  querySelector
+ * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
+ */
+export function removeClass(selector: ElementTarget, className: string): void;
+
+/**
+ * Toggling class to element.
+ * @param selector  querySelector
+ * @param className class name. If you want give multiple classes, separates classes by whitespace. e.g. 'first second'
+ */
+export function toggleClass(selector: ElementTarget, className: string): void;
+
+/**
+ * Hide element. It will set element's display to 'none'.
+ * @param selector querySelector
+ */
+export function hide(selector: ElementTarget): void;
+
+/**
+ * Show element. It will just remove 'display=none;'.
+ * @param selector querySelector
+ */
+export function show(selector: ElementTarget): void;
+
+/**
+ * Toggling show/hide element.
+ * @param selector querySelector
+ */
+export function toggleShow(selector: ElementTarget): void;
+
+/**
+ * Make a hidden input.
+ * @param  name  name attribute.
+ * @param  value value attribute.
+ * @return
+ */
+export function makeHiddenInput(name: string, value: string): HTMLInputElement;
+
+/**
+ * Makes hidden input, and append to target element. If input[name=".."] already exists, overwrite it.
+ * @param target querySelector
+ * @param name   name attribute.
+ * @param value  value attribute.
+ */
+export function appendHiddenInput(target: ElementTarget, name: string, value: string): void;
+
+//
+// Helper methods.
+//
+
+/**
+ * Filtering elements.
+ * @param  selector   querySelector
+ * @param  filter     filter string.
+ * @param  enableHTML using .innerHTML, default is false.
+ * @return            Hit number.
+ */
+export function filter(selector: ElementTarget, filter?: string, enableHTML?: boolean): number;
+
+/**
+ * Sorting elements.
+ * @param  elm     base element.
+ * @param  options options for ElementSorter.
+ * @return         sorted elements.
+ */
+export function sort(elm: ElementTarget, options?: SorterOptions): HTMLElement[];
+
+//
+// ElementFilter
+//
+
 declare interface FilterOptions {
   /** Enable to use `innerHTML`. Default is false, and than use `textContent`. */
   enableHTML?: boolean;
 }
 
-declare class ElementFilter {
+export class ElementFilter {
   elms: NodeList;
   filter: string;
   hit: number;
@@ -228,9 +226,10 @@ declare class ElementFilter {
   execute(): this;
 }
 
-/**
- * ElementSorter
- */
+//
+// ElementSorter
+//
+
 /** Types that can be as target items. */
 declare type ItemsSettable = string|NodeList|HTMLElement[];
 
@@ -250,7 +249,7 @@ declare interface SorterOptions {
   datasetName?: DatasetNames;
 }
 
-declare class ElementSorter {
+export class ElementSorter {
   elm: HTMLElement;
   items: HTMLElement[];
   options: SorterOptions;
